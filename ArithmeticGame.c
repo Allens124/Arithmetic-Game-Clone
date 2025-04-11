713d77c13b2f35ee39945e770fed4c2e01148061
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-04-11 04:18:25 -0400

    prompted the user to enter the minInt and the maxInt

--- ArithmeticGame.c
+++ ArithmeticGame.c
@@ -5,5 +5,10 @@ int main()
 {
     int score = 0;
     int minInt = 2, maxInt = 100;
+    printf("What is the addition range?\n");
+    printf("Lower limit: ");
+    scanf("%d", minInt);
+    printf("Upper limit: ");
+    scanf("%d", maxInt);
     return 0;
 }
