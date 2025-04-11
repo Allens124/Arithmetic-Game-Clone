31b64a9a7f10ade2cc94fa520dd56c40201a8670
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-04-11 04:27:35 -0400

    checking output

--- ArithmeticGame.c
+++ ArithmeticGame.c
@@ -8,32 +8,32 @@ int main()
     int minIntAdd = 2, maxIntAdd = 100;
     printf("What is the addition range?\n");
     printf("Lower limit: ");
-    scanf("%d", minIntAdd);
+    scanf("%d", &minIntAdd);
     printf("Upper limit: ");
-    scanf("%d", maxIntAdd);
+    scanf("%d", &maxIntAdd);
     while (maxIntAdd <= minIntAdd)
     {
         printf("Invalid input! Upper limit must be greater than lower limit.\n");
         printf("What is the addition range?\n");
         printf("Lower limit: ");
-        scanf("%d", minIntAdd);
+        scanf("%d", &minIntAdd);
         printf("Upper limit: ");
-        scanf("%d", maxIntAdd);
+        scanf("%d", &maxIntAdd);
     }
     int minIntMult = 2, maxIntMult = 100;
     printf("What is the multiplication range?\n");
     printf("Lower limit: ");
-    scanf("%d", minIntMult);
+    scanf("%d", &minIntMult);
     printf("Upper limit: ");
-    scanf("%d", maxIntMult);
+    scanf("%d", &maxIntMult);
     while (maxIntMult <= minIntMult)
     {
         printf("Invalid input! Upper limit must be greater than lower limit.\n");
         printf("What is the multiplication range?\n");
         printf("Lower limit: ");
-        scanf("%d", minIntMult);
+        scanf("%d", &minIntMult);
         printf("Upper limit: ");
-        scanf("%d", maxIntMult);
+        scanf("%d", &maxIntMult);
     }
     return 0;
 }
