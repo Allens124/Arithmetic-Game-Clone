1b55726ef9d4d85f86989639e50c79e994c024eb
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-04-11 04:21:45 -0400

    handling exceptions for when maxIntAdd <= minIntAdd

--- ArithmeticGame.c
+++ ArithmeticGame.c
@@ -4,11 +4,20 @@
 int main()
 {
     int score = 0;
-    int minInt = 2, maxInt = 100;
+    int minIntAdd = 2, maxIntAdd = 100;
     printf("What is the addition range?\n");
     printf("Lower limit: ");
-    scanf("%d", minInt);
+    scanf("%d", minIntAdd);
     printf("Upper limit: ");
-    scanf("%d", maxInt);
+    scanf("%d", maxIntAdd);
+    while (maxIntAdd <= minIntAdd)
+    {
+        printf("Invalid input! Upper limit must be greater than lower limit.\n");
+        printf("What is the addition range?\n");
+        printf("Lower limit: ");
+        scanf("%d", minIntAdd);
+        printf("Upper limit: ");
+        scanf("%d", maxIntAdd);
+    }
     return 0;
 }
